85c74fdb5c8ea8c432141e927411574e8485c852
M R <6248874+mrbrirup@users.noreply.github.com>
2019-06-06 06:05:04 +0100

    Refactor loading and inheritance for speed, async

--- system/inheritance.js
+++ system/inheritance.js
@@ -17,7 +17,7 @@ class {
      * @param {string or string[]} sources source for inheritance string is converted to string[]
      * @param {class} target class that will inherit from sources
      */
-    static applyInheritance(sources, target) {        
+    static async applyInheritance(sources, target) {
         if (sources === undefined || sources.length === 0) { return; }
         const
             targetPrototype = target.prototype,
@@ -26,25 +26,31 @@ class {
             toObject = system.Assembly.toObject,
             assembly = system.Assembly,
             dotReplaceRegex = new RegExp("\\.", "g");
-        (Array.isArray(sources) ? sources : [sources])
-            .forEach(async strSource => {
+        sources = Array.isArray(sources) ? sources : [sources];
+        const prms = [];
+        for (let sourceCounter = 0, sourcesCount = sources.length; sourceCounter < sourcesCount; sourceCounter++) {
+            prms.push(new Promise((resolve, reject) => {
+                const strSource = sources[sourceCounter];
                 let sourcePrototype = toObject(strSource).prototype;
-                if (sourcePrototype === undefined) {
-                    await assembly.loadClass(strSource);
-                    sourcePrototype = toObject(strSource).prototype;
-                }
-                if (sourcePrototype === undefined) { return; }
-                let properties = Object.getOwnPropertyNames(sourcePrototype);
-                for (let propertiesCounter = 0, propertiesCount = properties.length, propertyName; propertiesCounter < propertiesCount; propertiesCounter++) {
-                    propertyName = properties[propertiesCounter];
-                    if (!nonInheritable.includes(propertyName)) {
-                        Object.defineProperty(
-                            targetPrototype,
-                            (propertyName !== "ctor" && Object.getOwnPropertyDescriptor(targetPrototype, propertyName) === undefined) ? propertyName : `${strSource.replace(dotReplaceRegex, "_")}_${propertyName}`,
-                            Object.getOwnPropertyDescriptor(sourcePrototype, propertyName)
-                        );
-                    }
-                };
-            });
+                ((sourcePrototype === undefined) ? assembly.loadClass(strSource) : Promise.resolve())
+                    .then(() => {
+                        let sourcePrototype = toObject(strSource).prototype;
+                        if (sourcePrototype === undefined) { resolve();return; }
+                        const properties = Object.getOwnPropertyNames(sourcePrototype);
+                        for (let propertiesCounter = 0, propertiesCount = properties.length; propertiesCounter < propertiesCount; propertiesCounter++) {
+                            const propertyName = properties[propertiesCounter];
+                            if (!nonInheritable.includes(propertyName)) {
+                                Object.defineProperty(
+                                    targetPrototype,
+                                    (propertyName !== "ctor" && Object.getOwnPropertyDescriptor(targetPrototype, propertyName) === undefined) ? propertyName : `${strSource.replace(dotReplaceRegex, "_")}_${propertyName}`,
+                                    Object.getOwnPropertyDescriptor(sourcePrototype, propertyName)
+                                );
+                            }
+                        };
+                        resolve();
+                    })
+            }));
+        }
+        await Promise.all(prms);
     }
 }
